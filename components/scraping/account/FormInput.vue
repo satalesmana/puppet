c9f897003afba816d907bb015909f6ce754f1fe5
Sata Lesmana <lesmanasata@gmail.com>
2023-11-30 18:15:28 +0700

    form validation account

--- components/scraping/account/FormInput.vue
+++ components/scraping/account/FormInput.vue
@@ -2,6 +2,7 @@
 import { useScrapingAccountStore } from '~/stores/scrapingAccount';
 const scrapingAccount = useScrapingAccountStore();
 const optAccounttype = computed(() => scrapingAccount.getOptAccountType);
+const scrapingAccountFormRef = ref([]);
 
 const fetTchData = async () => {
   const { value } = await scrapingAccount.fetchScrapingAccount();
@@ -9,121 +10,160 @@ const fetTchData = async () => {
 };
 
 const onSubmit = async () => {
-  const formInput = scrapingAccount.getFormInput;
-  await scrapingAccount.submitUser(formInput);
+  try {
+    const formInput = scrapingAccount.getFormInput;
+    await scrapingAccount.submitUser(formInput);
+    onReset();
+    await fetTchData();
+  } catch (err) {}
+};
+
+const onReset = () => {
   scrapingAccount.clearFormInput();
-  fetTchData();
+  setTimeout(() => {
+    scrapingAccountFormRef.value.resetValidation();
+  });
+};
+
+const isValidEmail = (email) => {
+  const regex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/;
+  return regex.test(email);
 };
 </script>
 <template>
   <div class="q-pa-lg">
-    <q-card class="my-card">
-      <q-card-section>
-        <div class="text-h6">Scraping Account</div>
-      </q-card-section>
-      <q-separator />
-      <q-card-section>
-        <div class="row">
-          <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
-            <div class="row q-mb-sm items-center">
-              <div
-                class="text-right q-pr-md col-lg-4 col-md-4 col-sm-4 col-xs-12"
-              >
-                <label>
-                  <b>Name</b>
-                </label>
+    <q-form
+      ref="scrapingAccountFormRef"
+      class="q-gutter-md"
+      @submit="onSubmit"
+      @reset="onReset"
+    >
+      <q-card class="my-card">
+        <q-card-section>
+          <div class="text-h6">Scraping Account</div>
+        </q-card-section>
+        <q-separator />
+        <q-card-section>
+          <div class="row">
+            <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+              <div class="row q-mb-sm items-center">
+                <div
+                  class="text-right q-pr-md col-lg-4 col-md-4 col-sm-4 col-xs-12"
+                >
+                  <label>
+                    <b>Name</b>
+                  </label>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                  <span class="custom-input-32">
+                    <q-input
+                      v-model="scrapingAccount.formInput.name"
+                      outlined
+                      dense
+                      filled
+                      hide-bottom-space
+                      :rules="[
+                        (val) =>
+                          (val !== null && val !== '') || 'Please type name',
+                      ]"
+                    />
+                  </span>
+                </div>
               </div>
-              <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                <span class="custom-input-32">
-                  <q-input
-                    v-model="scrapingAccount.formInput.name"
-                    outlined
-                    dense
-                    filled
-                    hide-bottom-space
-                    requird
-                  />
-                </span>
-              </div>
-            </div>
 
-            <div class="row q-mb-sm items-center">
-              <div
-                class="text-right q-pr-md col-lg-4 col-md-4 col-sm-4 col-xs-12"
-              >
-                <label>
-                  <b>Type</b>
-                </label>
+              <div class="row q-mb-sm items-center">
+                <div
+                  class="text-right q-pr-md col-lg-4 col-md-4 col-sm-4 col-xs-12"
+                >
+                  <label>
+                    <b>Type</b>
+                  </label>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                  <span class="custom-input-32">
+                    <q-select
+                      v-model="scrapingAccount.formInput.type"
+                      outlined
+                      dense
+                      filled
+                      emit-value
+                      map-options
+                      hide-bottom-space
+                      requird
+                      :options="optAccounttype"
+                      :options-dense="false"
+                      :rules="[
+                        (val) =>
+                          (val !== null && val !== '') || 'Please select type',
+                      ]"
+                    />
+                  </span>
+                </div>
               </div>
-              <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                <span class="custom-input-32">
-                  <q-select
-                    v-model="scrapingAccount.formInput.type"
-                    outlined
-                    dense
-                    filled
-                    emit-value
-                    map-options
-                    hide-bottom-space
-                    requird
-                    :options="optAccounttype"
-                    :options-dense="false"
-                  />
-                </span>
-              </div>
-            </div>
 
-            <div class="row q-mb-sm items-center">
-              <div
-                class="text-right q-pr-md col-lg-4 col-md-4 col-sm-4 col-xs-12"
-              >
-                <label>
-                  <b>Email</b>
-                </label>
+              <div class="row q-mb-sm items-center">
+                <div
+                  class="text-right q-pr-md col-lg-4 col-md-4 col-sm-4 col-xs-12"
+                >
+                  <label>
+                    <b>Email</b>
+                  </label>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                  <span class="custom-input-32">
+                    <q-input
+                      v-model="scrapingAccount.formInput.email"
+                      outlined
+                      dense
+                      filled
+                      hide-bottom-space
+                      requird
+                      :rules="[
+                        (val) =>
+                          (val !== null && val !== '') || 'Please type email',
+                        (val) => isValidEmail(val) || 'Invalid email address',
+                      ]"
+                    />
+                  </span>
+                </div>
               </div>
-              <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                <span class="custom-input-32">
-                  <q-input
-                    v-model="scrapingAccount.formInput.email"
-                    outlined
-                    dense
-                    filled
-                    hide-bottom-space
-                    requird
-                  />
-                </span>
-              </div>
-            </div>
 
-            <div class="row q-mb-sm items-center">
-              <div
-                class="text-right q-pr-md col-lg-4 col-md-4 col-sm-4 col-xs-12"
-              >
-                <label>
-                  <b>Password</b>
-                </label>
-              </div>
-              <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                <span class="custom-input-32">
-                  <q-input
-                    v-model="scrapingAccount.formInput.password"
-                    outlined
-                    dense
-                    filled
-                    hide-bottom-space
-                    requird
-                  />
-                </span>
+              <div class="row q-mb-sm items-center">
+                <div
+                  class="text-right q-pr-md col-lg-4 col-md-4 col-sm-4 col-xs-12"
+                >
+                  <label>
+                    <b>Password</b>
+                  </label>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                  <span class="custom-input-32">
+                    <q-input
+                      v-model="scrapingAccount.formInput.password"
+                      outlined
+                      dense
+                      filled
+                      hide-bottom-space
+                      requird
+                      :rules="[
+                        (val) =>
+                          (val !== null && val !== '') ||
+                          'Please type password',
+                      ]"
+                    />
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </q-card-section>
-      <q-separator />
-      <q-card-actions align="right">
-        <q-btn color="red">Cancel</q-btn>
-        <q-btn color="primary" @click="onSubmit">Submit</q-btn>
-      </q-card-actions>
-    </q-card>
+        </q-card-section>
+        <q-separator />
+        <q-card-actions align="right">
+          <q-btn color="red" type="reset">Cancel</q-btn>
+          <q-btn color="primary" type="submit">Submit</q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-form>
   </div>
 </template>
--- stores/scrapingAccount/action.ts
+++ stores/scrapingAccount/action.ts
@@ -1,20 +1,59 @@
+import { Loading, QSpinnerFacebook, Dialog } from 'quasar';
+
 export const fetchScrapingAccount = async () => {
-  const { $useApiFetch } = useNuxtApp();
-  const { data: scrapingAccount } = await $useApiFetch('/api/scraping/account');
-  return scrapingAccount;
+  try {
+    Loading.show({
+      spinner: QSpinnerFacebook,
+      message: 'Loading fetch data...',
+    });
+
+    const { $useApiFetch } = useNuxtApp();
+    const { data: scrapingAccount } = await $useApiFetch(
+      '/api/scraping/account',
+    );
+    Loading.hide();
+    return scrapingAccount;
+  } catch (err) {
+    Loading.hide();
+    return err;
+  }
 };
 
 export const submitUser = async (payload: any) => {
-  const { $useApiFetch } = useNuxtApp();
-  const { data: scrapingAccount } = await $useApiFetch(
-    '/api/scraping/account',
-    {
-      method: 'post',
-      body: { ...payload },
-    },
-  );
+  try {
+    Loading.show({
+      spinner: QSpinnerFacebook,
+      message: 'Loading submit data...',
+    });
+    const { $useApiFetch } = useNuxtApp();
+    const { data: scrapingAccount, error: errSubmit } = await $useApiFetch(
+      '/api/scraping/account',
+      {
+        method: 'post',
+        body: { ...payload },
+      },
+    );
+    Loading.hide();
+    if (errSubmit.value !== null) {
+      throw errSubmit.value?.data;
+    }
 
-  return scrapingAccount;
+    Dialog.create({
+      title: 'Info',
+      message: scrapingAccount.value.message,
+      html: true,
+    });
+
+    return scrapingAccount;
+  } catch (err) {
+    Loading.hide();
+    Dialog.create({
+      title: 'Error',
+      message: `<span class="text-red">${err.message}</span>`,
+      html: true,
+    });
+    throw err;
+  }
 };
 
 export const deleteScrapingAccount = async (id: any) => {
