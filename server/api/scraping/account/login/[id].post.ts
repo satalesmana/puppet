35818b0daf3afc9db3f671ec02077cca44c64a77
Sata Lesmana <lesmanasata@gmail.com>
2023-12-28 23:26:03 +0700

    login page

--- server/api/scraping/account/login/[id].post.ts
+++ server/api/scraping/account/login/[id].post.ts
@@ -4,16 +4,21 @@ import type { ApiResponse } from '~/server/types/apiresponse.interface';
 export default defineEventHandler(async (event) => {
   try {
     const { params } = event.context;
-    const { email, password } = await ScrapingAccount.findById({
+    const { email, password, _id } = await ScrapingAccount.findById({
       _id: params?.id,
     });
 
-    const token = await jobstreetLoginAccount(email, password);
+    const token = await jobstreetLoginAccount(email, password, _id);
     if (token === '') {
       throw new Error('login gagal');
     }
 
     await ScrapingAccount.updateOne({ _id: params?.id }, { cookies: token });
+
+    setInterval(async () => {
+      const newToken = await jobstreetReloadAccount(_id);
+      await ScrapingAccount.updateOne({ _id }, { cookies: newToken });
+    }, 60000); // 6 menit
 
     return {
       data: [],
--- server/utils/jobstreet/login.ts
+++ server/utils/jobstreet/login.ts
@@ -1,29 +1,31 @@
 import { launch } from 'puppeteer';
-let page: any;
+const page: any = {};
+const browser: any = {};
 
 export const jobstreetLoginAccount = async (
   username: string,
   password: string,
+  _id: string,
 ) => {
-  const browser = await launch({
+  browser[_id] = await launch({
     headless: false,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     slowMo: 20,
   });
 
-  page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(1000000);
-  await page.setViewport({ width: 1000, height: 600 });
-  await page.goto('https://id.employer.seek.com/id/oauth/login/');
-  await page.waitForSelector('#emailAddress');
-  await page.type('#emailAddress', username);
-  await page.waitForSelector('#password');
-  await page.type('#password', password);
-  await page.click(`button[type='submit']`);
+  page[_id] = await browser[_id].newPage();
+  await page[_id].setDefaultNavigationTimeout(1000000);
+  await page[_id].setViewport({ width: 1000, height: 600 });
+  await page[_id].goto('https://id.employer.seek.com/id/oauth/login/');
+  await page[_id].waitForSelector('#emailAddress');
+  await page[_id].type('#emailAddress', username);
+  await page[_id].waitForSelector('#password');
+  await page[_id].type('#password', password);
+  await page[_id].click(`button[type='submit']`);
 
-  await page.waitForNavigation();
-  await page.waitForTimeout(10000);
-  const authSession = await page.evaluate(() => {
+  await page[_id].waitForNavigation();
+  await page[_id].waitForTimeout(10000);
+  const authSession = await page[_id].evaluate(() => {
     let token;
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i);
@@ -36,14 +38,15 @@ export const jobstreetLoginAccount = async (
     return token || '';
   });
 
-  browser.close();
+  // browser.close();
   return authSession;
 };
 
-export const jobstreetReloadAccount = async () => {
-  await page.waitForNavigation();
-  await page.waitForTimeout(10000);
-  const authSession = await page.evaluate(() => {
+export const jobstreetReloadAccount = async (_id: string) => {
+  await page[_id].reload({ waitUntil: ['networkidle0', 'domcontentloaded'] });
+  // await page[_id].waitForNavigation();
+  await page[_id].waitForTimeout(10000);
+  const authSession = await page[_id].evaluate(() => {
     let token;
     for (let i = 0; i < localStorage.length; i++) {
       const key = localStorage.key(i);
@@ -56,4 +59,8 @@ export const jobstreetReloadAccount = async () => {
     return token || '';
   });
   return authSession;
+};
+
+export const stopSession = async (_id: string) => {
+  await browser[_id].close();
 };
