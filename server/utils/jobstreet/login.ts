787802657187fb85dcfb125132379f48fcbee5c1
sata lesmana <lesmanasata@gmail.com>
2025-08-24 13:17:51 +0700

    login

--- server/utils/jobstreet/login.ts
+++ server/utils/jobstreet/login.ts
@@ -16,7 +16,8 @@ export const jobstreetLoginAccount = async (
   try{
 
     const config = useRuntimeConfig();
-    if (fs.existsSync(config.browserPath)) {
+    const useCustomeBrowser = fs.existsSync(config.browserPath);
+    if (useCustomeBrowser) {
       browser[_id] = await puppeteer.launch({
         headless: 'new',
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
@@ -24,24 +25,15 @@ export const jobstreetLoginAccount = async (
         slowMo: 20,
       });
     } else {
-      browser[_id] = await puppeteer.launch({
-        args: chromium.args,
-        ignoreDefaultArgs: ['--disable-extensions','--single-process'],
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(
-          "https://github.com/Sparticuz/chromium/releases/download/v110.0.1/chromium-v110.0.1-pack.tar"
-        ),
-        headless: chromium.headless,
-        ignoreHTTPSErrors: true,
+       browser[_id] = await puppeteer.launch({
+        headless: false,
+        args: [ '--start-maximized'],
+        channel: "chrome",
+        slowMo: 20,
       });
     }
 
     page[_id] = await browser[_id].newPage();
-
-    // await page[_id].setJavaScriptEnabled(true);
-    // await page[_id].setDefaultNavigationTimeout(1000000);
-    // useSleep();
-    // await page[_id].setViewport({ width: 1000, height: 600 });
     await page[_id].goto('https://id.employer.seek.com/id/oauth/login/',{ waitUntil: "networkidle0" });
 
     console.log("INFO Chromium:", await browser[_id].version());
@@ -55,11 +47,8 @@ export const jobstreetLoginAccount = async (
     // await page[_id].screenshot({ path: `./public/assets/screenshot/${_id}${new Date()}.jpg` });
     console.log("INFO Chromium:", await browser[_id].version());
     console.log("INFO Page Title:", await page[_id].title());
+    await page[_id].waitForSelector('table');
     useSleep();
-    // await page[_id].screenshot({ path: `./public/assets/screenshot/${_id}${new Date()}.jpg` });
-
-    // await page[_id].waitForNavigation();
-    // await page[_id].waitForTimeout(10000);
     const authSession = await page[_id].evaluate(() => {
       let token;
       for (let i = 0; i < localStorage.length; i++) {
@@ -67,7 +56,9 @@ export const jobstreetLoginAccount = async (
         if (key?.includes('auth')) {
           const json = localStorage.getItem(key) as string;
           const auth = JSON.parse(json);
-          token = auth.body.access_token;
+          if (auth.body && auth.body.access_token) {
+            token = auth.body.access_token;
+          }
         }
       }
       return token || '';
@@ -91,7 +82,9 @@ export const jobstreetReloadAccount = async (_id: string) => {
       if (key?.includes('auth')) {
         const json = localStorage.getItem(key) as string;
         const auth = JSON.parse(json);
-        token = auth.body.access_token;
+        if (auth.body && auth.body.access_token) {
+          token = auth.body.access_token;
+        }
       }
     }
     return token || '';
