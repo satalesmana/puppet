43ad53b6b4e7c59b8b05d5089f827dbcc4cace95
sata lesmana <lesmanasata@gmail.com>
2026-01-01 15:43:47 +0700

    fix fetch pelamar

--- server/api/scraping/task/biller/[id].post.ts
+++ server/api/scraping/task/biller/[id].post.ts
@@ -13,18 +13,15 @@ export default defineEventHandler(async (event) => {
     });
 
     const requestBody = {
-      operationName: 'HeaderFooterContext',
-      variables: {
-        headerFooterContextInput: {
-          location: {
-            pathname: '/id/candidates/',
-            search: `?id=${positionId}`,
-          },
-        },
+      "operationName": "AccountContext",
+      "variables": {
+          "skipAdvertiserContext": false,
+          "skipCanClaimCompanyProfilePermission": true,
+          "skipCanManageCompanyProfilePermission": true
       },
-      query:
-        'query HeaderFooterContext($headerFooterContextInput: HeaderFooterContextInput) {\n  headerFooterContext {\n    identityContext {\n      act(input: $headerFooterContextInput) {\n        operatorEmail\n        csToggleHref\n        isCsView\n        __typename\n      }\n      email\n      __typename\n    }\n    advertiser {\n      billingId\n      hasMultipleAccounts\n      isActivationPending\n      isAgency\n      name\n      permissions\n      showOnCreditHoldMessage\n      switchAccountsHref\n      __typename\n    }\n    user {\n      name\n      firstName\n      id\n      __typename\n    }\n    __typename\n  }\n}',
-    };
+      "query": "query AccountContext($skipAdvertiserContext: Boolean = false, $skipCanClaimCompanyProfilePermission: Boolean = true, $skipCanManageCompanyProfilePermission: Boolean = true) {\n  hirerOriginZone\n  currentAdvertiser @skip(if: $skipAdvertiserContext) {\n    ... on CurrentAdvertiserContext {\n      __typename\n      name\n      seekId\n      isAgency\n      billingId\n      permissionOptions {\n        ...PermissionOptionFields\n        canManageCompanyProfile @skip(if: $skipCanManageCompanyProfilePermission)\n        canClaimCompanyProfile @skip(if: $skipCanClaimCompanyProfilePermission)\n        __typename\n      }\n      status\n      role\n    }\n    ... on Error {\n      __typename\n      message\n    }\n    __typename\n  }\n  currentUser {\n    ... on CurrentUserContext {\n      __typename\n      id\n      email\n      firstName\n      lastName\n      name\n      status\n    }\n    ... on Error {\n      __typename\n      message\n    }\n    __typename\n  }\n  headerFooterContext {\n    __typename\n    identityContext {\n      email\n      act {\n        operatorEmail\n        isCsView\n        __typename\n      }\n      __typename\n    }\n    user {\n      name\n      firstName\n      id\n      status\n      __typename\n    }\n    advertiser @skip(if: $skipAdvertiserContext) {\n      name\n      billingId\n      hasMultipleAccounts\n      permissionOptions {\n        ...PermissionOptionFields\n        canManageCompanyProfile @skip(if: $skipCanManageCompanyProfilePermission)\n        canClaimCompanyProfile @skip(if: $skipCanClaimCompanyProfilePermission)\n        __typename\n      }\n      showOnCreditHoldMessage\n      isActivationPending\n      switchAccountsHref\n      isAgency\n      __typename\n    }\n  }\n}\n\nfragment PermissionOptionFields on PermissionOptions {\n  canCreateJobs\n  canManageBrand\n  canManageUsers\n  canViewInsightsRoleReport\n  canPayInvoices\n  canViewInvoiceHistory\n  canViewManagerReports\n  canViewPremiumTalentSearch\n  canManageJob\n  __typename\n}"
+    }
+
     const response = await fetch('https://id.employer.seek.com/graphql', {
       method: 'POST',
       headers: {
